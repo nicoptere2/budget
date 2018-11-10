9a1522f1b67b30562ffd72757ba310a1bc2cae35
Nicolas Weissenbach <nicolasweissenbach@nicolass-macbook-pro.local>
2018-11-10 11:46:33 +0100

    Initial Commit

---
+++ budget/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  budget
+//
+//  Created by Nicolas Weissenbach on 10/11/2018.
+//  Copyright © 2018 Nicolas Weissenbach. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
